12d18d9b01fe7441cb5d920284b687124d7263f6
Alex Wright <AlexParadiseWright+git@gmail.com>
2016-10-12 23:13:18 +0200

    Added more crypto hashes

--- app/src/main/java/com/itsabugnotafeature/securikey/crypto/Crypto.java
+++ app/src/main/java/com/itsabugnotafeature/securikey/crypto/Crypto.java
@@ -7,13 +7,26 @@ import java.security.NoSuchAlgorithmException;
  * Created by alex on 2016/10/08.
  */
 
+
+/**
+ * TODO - alhorithms return a buyte array,
+ *                  this provides much flexibility but we need to be smart about generating them
+ *
+ */
+
 public class Crypto {
 
-    public static final String md5(final String s) {
-        final String MD5 = "MD5";
+    final static String DEFAULT_ALGORITHM = "SHA-256";
+    final static Integer DEFAULT_ITERATIONS = 1;
+
+    public static final String getBasicHash(String s) {
+        return getBasicHash(s, DEFAULT_ALGORITHM);
+    }
+
+    public static final String getBasicHash(String s, String algorithm) {
         try {
             // Create MD5 Hash
-            MessageDigest digest = MessageDigest.getInstance(MD5);
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
             digest.update(s.getBytes());
             byte messageDigest[] = digest.digest();
 
@@ -28,9 +41,34 @@ public class Crypto {
             return hexString.toString();
 
         } catch (NoSuchAlgorithmException e) {
+            // TODO
             e.printStackTrace();
         }
         return "";
     }
+
+
+    public static final String md5(final String s) {
+        final String MD5 = "MD5";
+        return getBasicHash(s, MD5);
+    }
+
+    public static final String SHA1(final String s) {
+        final String SHA1 = "SHA-1";
+        return getBasicHash(s, SHA1);
+    }
+
+    public static final String SHA256(final String s) {
+        final String SHA256 = "SHA-256";
+        return getBasicHash(s, SHA256);
+    }
+
+
+    public static final String SHA512(final String s) {
+        final String SHA512 = "SHA-512";
+        return getBasicHash(s, SHA512);
+    }
+
+
 
 }
